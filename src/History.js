e232d05278d868bc879162ba777742270839cdfe
Rob Huehn <rhuehn@gmail.com>
2018-05-26 16:57:59 -0700

    write initial device state

--- src/History.js
+++ src/History.js
@@ -2,21 +2,21 @@ import {bus, proxy} from '@theatersoft/bus'
 import {create, write} from './db'
 
 export class History {
-    async start({name}) {
+    async start ({name}) {
         this.name = name
         await bus.registerObject(name, this)
         try {
-            this.influx = await create(name,)
-            bus.registerListener('Device.state',
-                ({devices}) => write(Object.values(devices), this.influx)
-            )
+            this.influx = await create(name)
+            const writeState = ({devices}) => write(devices, this.influx)
+            writeState(await proxy('Device').getState())
+            bus.registerListener('Device.state', writeState)
         } catch (e) {
             this.stop()
             throw e
         }
     }
 
-    stop() {
+    stop () {
         return bus.unregisterObject(this.name)
     }
 }
